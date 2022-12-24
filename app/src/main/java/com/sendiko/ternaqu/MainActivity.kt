ee65a7a5900fe0ec1ef837f9a0a557348d658bf1
Sendiko <sarangtawon897@gmail.com>
2022-12-24 21:26:59 +0700

    initial commit

---
+++ app/src/main/java/com/sendiko/ternaqu/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sendiko.ternaqu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sendiko.ternaqu.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
