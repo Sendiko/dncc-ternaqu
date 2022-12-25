873262be3a349bd6d51647f48faca5085224752d
Sendiko <sarangtawon897@gmail.com>
2022-12-25 20:07:23 +0700

    enhancement - implement view binding

--- app/src/main/java/com/sendiko/ternaqu/ui/container/MainActivity.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/container/MainActivity.kt
@@ -3,11 +3,15 @@ package com.sendiko.ternaqu.ui.container
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sendiko.ternaqu.R
+import com.sendiko.ternaqu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
