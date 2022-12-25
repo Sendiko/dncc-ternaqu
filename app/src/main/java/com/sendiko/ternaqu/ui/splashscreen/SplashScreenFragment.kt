6b47af4642dc764f5fb80d725dfb14eb27d02471
Sendiko <sarangtawon897@gmail.com>
2022-12-25 20:09:59 +0700

    enhancement - implement view binding

--- app/src/main/java/com/sendiko/ternaqu/ui/splashscreen/SplashScreenFragment.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/splashscreen/SplashScreenFragment.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sendiko.ternaqu.R
+import com.sendiko.ternaqu.databinding.FragmentSplashScreenBinding
 
 class SplashScreenFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    ): View {
+        binding = FragmentSplashScreenBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
