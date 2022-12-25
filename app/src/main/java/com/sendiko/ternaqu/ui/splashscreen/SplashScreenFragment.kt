9f747b0ba6174c34a5780577f7b0e0bd549c962d
Sendiko <sarangtawon897@gmail.com>
2022-12-25 21:19:28 +0700

    enhancement - implement delay method

--- app/src/main/java/com/sendiko/ternaqu/ui/splashscreen/SplashScreenFragment.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/splashscreen/SplashScreenFragment.kt
@@ -1,10 +1,14 @@
 package com.sendiko.ternaqu.ui.splashscreen
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.sendiko.ternaqu.R
 import com.sendiko.ternaqu.databinding.FragmentSplashScreenBinding
 
@@ -17,6 +21,11 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSplashScreenBinding.inflate(layoutInflater)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            this.findNavController().navigate(R.id.action_splashScreenFragment2_to_walkthroughFragment)
+        }, 800)
+
         return binding.root
     }
 }
