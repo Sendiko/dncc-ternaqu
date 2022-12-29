c52d74ad7f789c7d1b818507f06b726e37541680
Sendiko <sarangtawon897@gmail.com>
2022-12-29 22:00:46 +0700

    enhancement - create deshboard fragment

--- app/src/main/java/com/sendiko/ternaqu/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/dashboard/DashboardFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sendiko.ternaqu.R
 import com.sendiko.ternaqu.databinding.FragmentDashboardBinding
 
@@ -22,6 +23,10 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvResep.layoutManager = layoutManager
+        
         
     }
 
