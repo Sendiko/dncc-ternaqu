d1185cc1eea5045f9b52f719b1e7524540476bed
Sendiko <sarangtawon897@gmail.com>
2022-12-31 13:56:49 +0700

    refactor - moved packages

--- app/src/main/java/com/sendiko/ternaqu/repository/auth/AuthViewModel.kt
+++ app/src/main/java/com/sendiko/ternaqu/repository/auth/AuthViewModel.kt
@@ -1,9 +1,20 @@
 package com.sendiko.ternaqu.repository
 
 import androidx.lifecycle.*
+import com.sendiko.ternaqu.repository.auth.AuthPreferences
 import kotlinx.coroutines.launch
 
 class AuthViewModel(private val auth : AuthPreferences) : ViewModel() {
+
+    fun getTokenAccess() : LiveData<String> {
+        return auth.getTokenAccess().asLiveData()
+    }
+
+    fun saveTokenAccess(token : String){
+        viewModelScope.launch {
+            auth.saveTokenAccess(token)
+        }
+    }
 
     fun getLoginState() : LiveData<Boolean> {
         return auth.getLoginState().asLiveData()
