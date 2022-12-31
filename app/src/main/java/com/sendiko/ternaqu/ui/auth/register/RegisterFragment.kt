16b1cf0e341ecb46b9cb6336c72c9197cfd56213
Sendiko <sarangtawon897@gmail.com>
2022-12-31 14:20:40 +0700

    enhancement - implement register api

--- app/src/main/java/com/sendiko/ternaqu/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/auth/register/RegisterFragment.kt
@@ -2,21 +2,41 @@ package com.sendiko.ternaqu.ui.auth.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentResultListener
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.sendiko.ternaqu.R
 import com.sendiko.ternaqu.databinding.FragmentRegisterBinding
+import com.sendiko.ternaqu.network.request.LoginRequest
+import com.sendiko.ternaqu.network.request.RegisterRequest
+import com.sendiko.ternaqu.repository.ViewModelFactory
+import com.sendiko.ternaqu.repository.user.UserViewModel
 import com.sendiko.ternaqu.ui.container.WelcomeActivity
+import com.sendiko.ternaqu.ui.loading.LoadingDialogFragment
+
+private const val TAG = "RegisterFragment"
 
 class RegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
+
+    private fun obtainViewModel(activity: FragmentActivity): UserViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(this, factory)[UserViewModel::class.java]
+    }
+
+    private val userViewModel by lazy {
+        obtainViewModel(requireNotNull(this.activity))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,12 +62,49 @@ class RegisterFragment : Fragment() {
             val email = binding.inputEmail.text.toString()
             val password = binding.inputPassword.text.toString()
             val passwordConfirmation = binding.inputConfirmPassword.text.toString()
-            when(validate(name, email, password, passwordConfirmation)){
-                true -> Toast.makeText(context, "valid", Toast.LENGTH_SHORT).show()
-                else -> {}
+            Log.e(TAG, "onViewCreated: $name, $email, $password, $passwordConfirmation")
+            when {
+                validate(name, email, password, passwordConfirmation) ->
+                    userViewModel.postRegister(
+                        RegisterRequest(
+                            name, email, password, passwordConfirmation
+                        )
+                    ).observe(viewLifecycleOwner) {
+                        when {
+                            it -> findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+                        }
+                    }
             }
         }
 
+        userViewModel.isFailed.observe(viewLifecycleOwner) {
+            when {
+                it.isFailed -> showSnackbar(it.failedMessage)
+            }
+        }
+
+        userViewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        var loadingDialogFragment = LoadingDialogFragment()
+        when {
+            isLoading -> {
+                loadingDialogFragment.show(parentFragmentManager)
+            }
+            else -> {
+                loadingDialogFragment =
+                    parentFragmentManager.findFragmentByTag(LoadingDialogFragment().FRAGMENT_TAG) as LoadingDialogFragment
+                loadingDialogFragment.dismiss()
+            }
+        }
     }
 
     private fun validate(
