57d9b91b8c3e4d9f6f61831c8f8c910f58c37219
Sendiko <sarangtawon897@gmail.com>
2023-01-02 09:32:44 +0700

    enhancement - create recipeviewmodel

--- app/src/main/java/com/sendiko/ternaqu/repository/ViewModelFactory.kt
+++ app/src/main/java/com/sendiko/ternaqu/repository/ViewModelFactory.kt
@@ -3,20 +3,21 @@ package com.sendiko.ternaqu.repository
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.sendiko.ternaqu.repository.recipe.RecipeViewModel
 import com.sendiko.ternaqu.repository.user.UserViewModel
 
-class ViewModelFactory private constructor(private val application: Application) :
+class ViewModelFactory private constructor(private val app: Application) :
     ViewModelProvider.NewInstanceFactory() {
     companion object {
         @Volatile
         private var INSTANCE: ViewModelFactory? = null
 
         @JvmStatic
-        fun getInstance(application: Application): ViewModelFactory {
+        fun getInstance(app: Application): ViewModelFactory {
             when (INSTANCE) {
                 null -> {
                     synchronized(ViewModelFactory::class.java) {
-                        INSTANCE = ViewModelFactory(application)
+                        INSTANCE = ViewModelFactory(app)
                     }
                 }
             }
@@ -26,7 +27,8 @@ class ViewModelFactory private constructor(private val application: Application)
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
-            modelClass.isAssignableFrom(UserViewModel::class.java) -> UserViewModel(application) as T
+            modelClass.isAssignableFrom(UserViewModel::class.java) -> UserViewModel(app) as T
+            modelClass.isAssignableFrom(RecipeViewModel::class.java) -> RecipeViewModel(app) as T
             else -> throw IllegalArgumentException("Unknown model class : " + modelClass.name)
         }
     }
---
+++ app/src/main/java/com/sendiko/ternaqu/repository/recipe/RecipeViewModel.kt
@@ -0,0 +1,80 @@
+package com.sendiko.ternaqu.repository.recipe
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.sendiko.ternaqu.network.response.RecipeResponse
+import com.sendiko.ternaqu.repository.model.FailedMessage
+import com.sendiko.ternaqu.repository.model.Recipe
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+private const val TAG = "RecipeViewModel"
+class RecipeViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val repo = RecipeRepository(app)
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isFailed = MutableLiveData<FailedMessage>()
+    val isFailed: LiveData<FailedMessage> = _isFailed
+
+    fun getRecipe(): LiveData<ArrayList<Recipe>> {
+        _isLoading.value = true
+        val resultRecipe = MutableLiveData<ArrayList<Recipe>>()
+        val recipeList = ArrayList<Recipe>()
+        val request = repo.getRecipe()
+        request.enqueue(
+            object : Callback<RecipeResponse>{
+                override fun onResponse(
+                    call: Call<RecipeResponse>,
+                    response: Response<RecipeResponse>
+                ) {
+                    _isLoading.value = false
+                    when(response.code()){
+                        200 -> {
+                            when(response.body()){
+                                null -> _isFailed.value = FailedMessage(true, "Server error.")
+                                else -> {
+                                    for(i in response.body()!!.recipe!!){
+                                        when(i){
+                                            null -> _isFailed.value = FailedMessage(true, "Data empty.")
+                                            else -> {
+                                                val recipe = Recipe(
+                                                    i.id?:0,
+                                                    i.title?:"",
+                                                    i.benefit?:"",
+                                                    i.toolsAndMaterials?:"",
+                                                    i.steps?:"",
+                                                    i.imageUrl?:""
+                                                    )
+                                                recipeList.add(recipe)
+                                                resultRecipe.value = recipeList
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else -> {
+                            _isFailed.value = FailedMessage(true, "Server error.")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<RecipeResponse>, t: Throwable) {
+                    _isLoading.value = false
+                    _isFailed.value = FailedMessage(true, "${t.message}")
+                    Log.e(TAG, "onFailure: ${t.message}")
+                }
+            }
+
+        )
+        return resultRecipe
+    }
+
+}
