ae34d09500c0017725fd6e078b40261627f17cac
Sendiko <sarangtawon897@gmail.com>
2023-01-03 13:39:18 +0700

    enhancement - retrieve data from api

---
+++ app/src/main/java/com/sendiko/ternaqu/network/response/ProductResponse.kt
@@ -0,0 +1,51 @@
+package com.sendiko.ternaqu.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductResponse(
+
+	@field:SerializedName("product")
+	val product: List<ProductItem?>? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("status")
+	val status: Int? = null
+)
+
+data class ProductItem(
+
+	@field:SerializedName("benefits")
+	val benefits: String? = null,
+
+	@field:SerializedName("store_id")
+	val storeId: Int? = null,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("price")
+	val price: Int? = null,
+
+	@field:SerializedName("product_id")
+	val productId: String? = null,
+
+	@field:SerializedName("imageUrl")
+	val imageUrl: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("brand")
+	val brand: String? = null
+)
