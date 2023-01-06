b9c23e6d13ac84553737ca569e01f4943fc0463e
Sendiko <sarangtawon897@gmail.com>
2023-01-06 22:00:56 +0700

    enhancement - request and response adjustment

---
+++ app/src/main/java/com/sendiko/ternaqu/network/request/TopicRequest.kt
@@ -0,0 +1,21 @@
+package com.sendiko.ternaqu.network.request
+
+import com.google.gson.annotations.SerializedName
+
+data class TopicRequest(
+
+	@field:SerializedName("user_id")
+	val userId: Int? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("question")
+	val question: String? = null,
+
+	@field:SerializedName("replyTo")
+	val replyTo: Int? = null,
+
+	@field:SerializedName("profileUrl")
+	val profileUrl: String? = null,
+)
