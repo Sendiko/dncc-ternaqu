ac3868a8ef384aff7eb112b7c3b3208c3278743f
Sendiko <sarangtawon897@gmail.com>
2023-01-07 23:15:45 +0700

    enhancement - create product list fragment

---
+++ app/src/main/java/com/sendiko/ternaqu/ui/product/ProductListAdapter.kt
@@ -0,0 +1,40 @@
+package com.sendiko.ternaqu.ui.product
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sendiko.ternaqu.databinding.CardListItemBinding
+import com.sendiko.ternaqu.network.response.ProductItem
+
+class ProductListAdapter(
+    private val product: ArrayList<ProductItem>,
+    private val context: Context
+) : RecyclerView.Adapter<ProductListAdapter.ProductListViewHolder>() {
+
+    class ProductListViewHolder(var binCardListItemBinding: CardListItemBinding) :
+        RecyclerView.ViewHolder(binCardListItemBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductListViewHolder {
+        val binding =
+            CardListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProductListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductListViewHolder, position: Int) {
+        val currentItem = product[position]
+        holder.binCardListItemBinding.textView15.text = currentItem.title
+        holder.binCardListItemBinding.textView17.text = currentItem.description
+        holder.binCardListItemBinding.textView25.text = currentItem.price.toString()
+        Glide.with(context)
+            .load(currentItem.imageUrl)
+            .circleCrop()
+            .into(holder.binCardListItemBinding.imageView8)
+    }
+
+    override fun getItemCount(): Int {
+        return product.size
+    }
+
+}
---
+++ app/src/main/java/com/sendiko/ternaqu/ui/product/ProductListFragment.kt
@@ -0,0 +1,54 @@
+package com.sendiko.ternaqu.ui.product
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sendiko.ternaqu.R
+import com.sendiko.ternaqu.databinding.FragmentProductListBinding
+import com.sendiko.ternaqu.repository.helper.ViewModelFactory
+import com.sendiko.ternaqu.repository.product.ProductViewModel
+
+class ProductListFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductListBinding
+
+    private fun obtainProductViewModel(activity: FragmentActivity): ProductViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(this, factory)[ProductViewModel::class.java]
+    }
+
+    private val productViewModel by lazy {
+        obtainProductViewModel(requireNotNull(this.activity))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProductListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navBack.setOnClickListener {
+            findNavController().navigate(R.id.action_productListFragment_to_dashboardFragment)
+        }
+
+        productViewModel.getProduct().observe(viewLifecycleOwner){
+            binding.rvProductsList.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = ProductListAdapter(it, requireContext())
+            }
+        }
+
+    }
+
+}
