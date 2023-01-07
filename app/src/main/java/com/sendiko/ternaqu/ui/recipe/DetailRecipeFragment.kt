055e64595b358e3a662b6f0e4d73795565e65610
Sendiko <sarangtawon897@gmail.com>
2023-01-07 23:50:08 +0700

    enhancement - create detail recipe

---
+++ app/src/main/java/com/sendiko/ternaqu/ui/recipe/DetailRecipeFragment.kt
@@ -0,0 +1,47 @@
+package com.sendiko.ternaqu.ui.recipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.sendiko.ternaqu.databinding.FragmentDetailRecipeBinding
+import com.sendiko.ternaqu.repository.helper.SharedViewModel
+
+class DetailRecipeFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailRecipeBinding
+
+    private val sharedViewModel : SharedViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailRecipeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedViewModel.recipe.observe(viewLifecycleOwner){
+            Glide.with(requireContext())
+                .load(it.imageUrl)
+                .transform(CenterInside(), RoundedCorners(16))
+                .into(binding.imageView12)
+
+            binding.textView26.text = it.title
+            binding.textView27.text = it.benefit
+            binding.textView29.text = it.toolsAndMaterials
+            binding.textView31.text = it.steps
+
+        }
+
+    }
+
+}
