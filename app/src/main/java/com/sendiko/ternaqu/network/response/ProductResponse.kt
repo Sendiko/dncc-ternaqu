380f7972fef53eec8d828639fa1e0f05cf8441ea
Sendiko <sarangtawon897@gmail.com>
2023-01-08 08:47:01 +0700

    enhancement - add product response

--- app/src/main/java/com/sendiko/ternaqu/network/response/ProductResponse.kt
+++ app/src/main/java/com/sendiko/ternaqu/network/response/ProductResponse.kt
@@ -4,8 +4,11 @@ import com.google.gson.annotations.SerializedName
 
 data class ProductResponse(
 
-	@field:SerializedName("product")
-	val product: List<ProductItem?>? = null,
+	@field:SerializedName("recipe")
+	val recipe: Recipe? = null,
+
+	@field:SerializedName("store")
+	val store: Store? = null,
 
 	@field:SerializedName("message")
 	val message: String? = null,
@@ -14,7 +17,31 @@ data class ProductResponse(
 	val status: Int? = null
 )
 
-data class ProductItem(
+data class Recipe(
+
+	@field:SerializedName("benefits")
+	val benefits: String? = null,
+
+	@field:SerializedName("store_id")
+	val storeId: Int? = null,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("price")
+	val price: Int? = null,
+
+	@field:SerializedName("product_id")
+	val productId: String? = null,
+
+	@field:SerializedName("imageUrl")
+	val imageUrl: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
 
 	@field:SerializedName("id")
 	val id: Int? = null,
@@ -23,29 +50,29 @@ data class ProductItem(
 	val title: String? = null,
 
 	@field:SerializedName("brand")
-	val brand: String? = null,
+	val brand: String? = null
+)
 
-	@field:SerializedName("description")
-	val description: String? = null,
-
-	@field:SerializedName("benefits")
-	val benefits: String? = null,
-
-	@field:SerializedName("price")
-	val price: Int? = null,
+data class Store(
 
 	@field:SerializedName("store_id")
-	val storeId: Int? = null,
+	val storeId: String? = null,
 
-	@field:SerializedName("product_id")
-	val productId: String? = null,
-
-	@field:SerializedName("imageUrl")
-	val imageUrl: String? = null,
+	@field:SerializedName("address")
+	val address: String? = null,
 
 	@field:SerializedName("updated_at")
 	val updatedAt: String? = null,
 
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null,
+
 	@field:SerializedName("created_at")
 	val createdAt: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null
 )
