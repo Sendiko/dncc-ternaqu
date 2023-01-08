c70d56f756a9f64fbc0809e9fdd7738a2e7a21fa
Sendiko <sarangtawon897@gmail.com>
2023-01-08 14:57:09 +0700

    enhancement - bug fix

--- app/src/main/java/com/sendiko/ternaqu/ui/product/ProductListFragment.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/product/ProductListFragment.kt
@@ -10,14 +10,12 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
 import com.sendiko.ternaqu.R
 import com.sendiko.ternaqu.databinding.FragmentProductListBinding
 import com.sendiko.ternaqu.network.response.ProductItem
 import com.sendiko.ternaqu.repository.helper.SharedViewModel
 import com.sendiko.ternaqu.repository.helper.ViewModelFactory
 import com.sendiko.ternaqu.repository.product.ProductViewModel
-import com.sendiko.ternaqu.ui.loading.LoadingDialogFragment
 
 class ProductListFragment : Fragment() {
 
@@ -65,35 +63,6 @@ class ProductListFragment : Fragment() {
             }
         }
 
-        productViewModel.isFailed.observe(viewLifecycleOwner) {
-            when {
-                it.isFailed -> showSnackbar(it.failedMessage)
-            }
-        }
-
-        productViewModel.isLoading.observe(viewLifecycleOwner) {
-            showLoading(it)
-        }
-
-
-    }
-
-    private fun showSnackbar(message: String) {
-        Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
-    }
-
-    private fun showLoading(isLoading: Boolean) {
-        var loadingDialogFragment = LoadingDialogFragment()
-        when {
-            isLoading -> {
-                loadingDialogFragment.show(parentFragmentManager)
-            }
-            else -> {
-                loadingDialogFragment =
-                    parentFragmentManager.findFragmentByTag(LoadingDialogFragment().FRAGMENT_TAG) as LoadingDialogFragment
-                loadingDialogFragment.dismiss()
-            }
-        }
     }
 
 }
