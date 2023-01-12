eb7c308c786c67bad605c42e42c9eba4dc0ceff2
Sendiko <sarangtawon897@gmail.com>
2023-01-12 08:42:19 +0700

    enhancement - upgrade to premium

--- app/src/main/java/com/sendiko/ternaqu/ui/user/UpgradeFragment.kt
+++ app/src/main/java/com/sendiko/ternaqu/ui/user/UpgradeFragment.kt
@@ -6,14 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.sendiko.ternaqu.R
 import com.sendiko.ternaqu.databinding.FragmentUpgradeBinding
 import com.sendiko.ternaqu.repository.AuthViewModel
 import com.sendiko.ternaqu.repository.AuthViewModelFactory
 import com.sendiko.ternaqu.repository.auth.AuthPreferences
+import com.sendiko.ternaqu.repository.helper.ViewModelFactory
+import com.sendiko.ternaqu.repository.user.UserViewModel
 import com.sendiko.ternaqu.ui.auth.dataStore
+import com.sendiko.ternaqu.ui.loading.LoadingDialogFragment
 
 class UpgradeFragment : Fragment() {
 
@@ -25,6 +30,15 @@ class UpgradeFragment : Fragment() {
 
     private val authViewModel: AuthViewModel by lazy {
         ViewModelProvider(this, AuthViewModelFactory(pref))[AuthViewModel::class.java]
+    }
+
+    private fun obtainViewModel(activity: FragmentActivity): UserViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(this, factory)[UserViewModel::class.java]
+    }
+
+    private val userViewModel by lazy {
+        obtainViewModel(requireNotNull(this.activity))
     }
 
     override fun onCreateView(
@@ -40,19 +54,68 @@ class UpgradeFragment : Fragment() {
 
         onBackPressed()
 
+        userViewModel.isFailed.observe(viewLifecycleOwner) {
+            when {
+                it.isFailed -> showSnackbar(it.failedMessage)
+            }
+        }
+
+        userViewModel.isLoading.observe(viewLifecycleOwner) {
+            showLoading(it)
+        }
+
         binding.navBack.setOnClickListener {
             findNavController().navigate(R.id.action_upgradeFragment_to_profileFragment)
         }
 
         binding.cardView.setOnClickListener {
-            authViewModel.savePremiumStatus("1")
-            findNavController().navigate(R.id.action_upgradeFragment_to_profileFragment)
+            authViewModel.getUserID().observe(viewLifecycleOwner){ userId ->
+                authViewModel.getTokenAccess().observe(viewLifecycleOwner){ token ->
+                    userViewModel.upgradeToPremium(userId.toString(), token).observe(viewLifecycleOwner){ response ->
+                        when (response.status) {
+                            200 -> {
+                                findNavController().navigate(R.id.action_upgradeFragment_to_profileFragment)
+                                showSnackbar(response.message!!)
+                            }
+                        }
+                    }
+                }
+            }
         }
         binding.cardView2.setOnClickListener {
-            authViewModel.savePremiumStatus("1")
-            findNavController().navigate(R.id.action_upgradeFragment_to_profileFragment)
+            authViewModel.getUserID().observe(viewLifecycleOwner){ userId ->
+                authViewModel.getTokenAccess().observe(viewLifecycleOwner){ token ->
+                    userViewModel.upgradeToPremium(userId.toString(), token).observe(viewLifecycleOwner){ response ->
+                        when (response.status) {
+                            200 -> {
+                                findNavController().navigate(R.id.action_upgradeFragment_to_profileFragment)
+                                showSnackbar(response.message!!)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        var loadingDialogFragment = LoadingDialogFragment()
+        when {
+            isLoading -> {
+                loadingDialogFragment.show(parentFragmentManager)
+            }
+            else -> {
+                loadingDialogFragment =
+                    parentFragmentManager.findFragmentByTag(LoadingDialogFragment().FRAGMENT_TAG) as LoadingDialogFragment
+                loadingDialogFragment.dismiss()
+            }
+        }
+    }
+
 
     private fun onBackPressed() {
         requireActivity().onBackPressedDispatcher.addCallback(this) {
